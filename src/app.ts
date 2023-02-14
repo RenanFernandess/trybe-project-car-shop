c250fd68be35f4234d40f019ad3911ceaca7656f
Renan Fernandes <renzinestuods@gmail.com>
2023-02-14 14:46:15 -0300

    :sparkles: feat: Usa o middleware errorRequestHandler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorRequestHandler from './Middlewares';
 import carRouter from './Routers';
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/cars', carRouter);
+
+app.use(errorRequestHandler);
 
 export default app;
