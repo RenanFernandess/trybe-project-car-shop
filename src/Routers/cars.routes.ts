4f1a4828a109c212bf76e8275bcaabc3648b8b93
Renan Fernandes <renzinestuods@gmail.com>
2023-02-14 15:18:32 -0300

    :sparkles: feat: Cria a rota GET '/' e '/:id'

--- src/Routers/cars.routes.ts
+++ src/Routers/cars.routes.ts
@@ -8,5 +8,7 @@ const carController = new CarController(carService);
 const carRouter = Router();
 
 carRouter.post('/', (req, res, next) => carController.create(req, res, next));
+carRouter.get('/', (req, res, next) => carController.findAll(req, res, next));
+carRouter.get('/:id', (req, res, next) => carController.findById(req, res, next));
 
 export default carRouter;
