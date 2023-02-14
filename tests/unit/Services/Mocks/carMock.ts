1660f9ba58fc33dd098b174b56292312d0e93e0b
Renan Fernandes <renzinestuods@gmail.com>
2023-02-14 17:22:52 -0300

    :test_tube: test: Cria os mock da model CarODM

---
+++ tests/unit/Services/Mocks/carMock.ts
@@ -0,0 +1,49 @@
+const createdMock = {
+  model: 'Marea xablau',
+  year: 2002,
+  color: 'Black-white',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+  _id: '63ebe8447220ca4cc0b49825',
+  __v: 0,
+};
+export default createdMock;
+
+export const findAllMock = [
+  {
+    _id: '63ebe8447220ca4cc0b49825',
+    model: 'Marea xablau',
+    year: 2002,
+    color: 'Black-white',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+    __v: 0,
+  },
+  {
+    _id: '63ebea916f281a62ecf64b30',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+    __v: 0,
+  },
+];
+
+export const findByIdMock = {
+  _id: '63ebe8447220ca4cc0b49825',
+  model: 'Marea black',
+  year: 2002,
+  color: 'white',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+  __v: 0,
+};
