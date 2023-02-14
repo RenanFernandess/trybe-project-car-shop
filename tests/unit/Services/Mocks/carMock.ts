bcf573efd3d59f5661cc098607becc4e776a079c
Renan Fernandes <renzinestuods@gmail.com>
2023-02-14 17:48:13 -0300

    :test_tube: test: Cria o mock carBodyMock

--- tests/unit/Services/Mocks/carMock.ts
+++ tests/unit/Services/Mocks/carMock.ts
@@ -1,28 +1,23 @@
-const createdMock = {
+export const carBodyMock = {
   model: 'Marea xablau',
   year: 2002,
   color: 'Black-white',
-  status: true,
-  buyValue: 15.99,
+  status: false,
+  buyValue: 15.990,
   doorsQty: 4,
   seatsQty: 5,
+};
+
+const createdMock = {
+  ...carBodyMock,
   _id: '63ebe8447220ca4cc0b49825',
   __v: 0,
 };
+
 export default createdMock;
 
 export const findAllMock = [
-  {
-    _id: '63ebe8447220ca4cc0b49825',
-    model: 'Marea xablau',
-    year: 2002,
-    color: 'Black-white',
-    status: true,
-    buyValue: 15.99,
-    doorsQty: 4,
-    seatsQty: 5,
-    __v: 0,
-  },
+  createdMock,
   {
     _id: '63ebea916f281a62ecf64b30',
     model: 'Marea',
@@ -36,14 +31,4 @@ export const findAllMock = [
   },
 ];
 
-export const findByIdMock = {
-  _id: '63ebe8447220ca4cc0b49825',
-  model: 'Marea black',
-  year: 2002,
-  color: 'white',
-  status: true,
-  buyValue: 15.99,
-  doorsQty: 4,
-  seatsQty: 5,
-  __v: 0,
-};
+export const findByIdMock = createdMock;
