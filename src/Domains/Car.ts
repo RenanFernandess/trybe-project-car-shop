6b226e9c7384ea9a94d044acd2d8e6d31990973b
Renan Fernandes <renzinestuods@gmail.com>
2023-02-13 21:02:05 -0300

    :truck: chore: Move o dominio Car para a pasta Domains

---
+++ src/Domains/Car.ts
@@ -0,0 +1,25 @@
+import ICar from '../Interfaces';
+
+export default class Car {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor({
+    model, year, color, status, buyValue, doorsQty, seatsQty, id,
+  }: ICar) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status || false;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+}
