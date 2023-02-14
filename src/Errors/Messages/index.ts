e7687812dfd4886cf18874f2b75104b1bbe2f915
Renan Fernandes <renzinestuods@gmail.com>
2023-02-14 14:45:16 -0300

    :sparkles: feat: Cria o objeto UNPROCESSABLE_ENTITY para message de erros do tipo 422

--- src/Errors/Messages/index.ts
+++ src/Errors/Messages/index.ts
@@ -1,6 +1,12 @@
 const NOT_FOUND = {
   status: 404,
   INVALID_ID: 'Wrong id format',
+  CAR_NOT_FOUND: 'Car not found',
 };
 
 export default NOT_FOUND;
+
+export const UNPROCESSABLE_ENTITY = {
+  status: 422,
+  MONGO_ID: 'Invalid mongo id',
+};
