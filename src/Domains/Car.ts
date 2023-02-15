6fd61aa5c0cf2b7ee0a08d427a6a12f2212fe962
Renan Fernandes <renzinestuods@gmail.com>
2023-02-15 20:47:54 -0300

    :boom: fix: Deixa de estender a class de dominio Vehicle e adiciona todos os atributos do Vehicle

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,12 +1,24 @@
 import ICar from '../Interfaces';
-import Vehicle from './Vehicle';
 
-export default class Car extends Vehicle {
+export default class Car {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
   private doorsQty: number;
   private seatsQty: number;
 
-  constructor({ doorsQty, seatsQty, ...car }: ICar) {
-    super(car);
+  constructor({
+    doorsQty, seatsQty, model, year, color, status, buyValue, id,
+  }: ICar) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
     this.doorsQty = doorsQty;
     this.seatsQty = seatsQty;
   }
--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,12 +1,24 @@
 import { IMotorcycle } from '../Interfaces';
-import Vehicle from './Vehicle';
 
-export default class Motorcycle extends Vehicle {
+export default class Motorcycle {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
   private category: string;
   private engineCapacity: number;
 
-  constructor({ category, engineCapacity, ...motorcycle }: IMotorcycle) {
-    super(motorcycle);
+  constructor({
+    category, engineCapacity, model, year, color, status, buyValue, id,
+  }: IMotorcycle) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
     this.category = category;
     this.engineCapacity = engineCapacity;
   }
