cf9741b63a9e1196734ee4ed67c44d545a2592ac
Renan Fernandes <renzinestuods@gmail.com>
2023-02-15 20:56:23 -0300

    :sparkles: feat: Usa o motorcycleRouter na rota '/motorcycles'

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import errorRequestHandler from './Middlewares';
-import carRouter from './Routers';
+import carRouter, { motorcycleRouter } from './Routers';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 
 app.use(errorRequestHandler);
 
